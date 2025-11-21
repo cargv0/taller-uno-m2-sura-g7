02e2062405a00606c8d6674d96303474fa0a5f6f
cargv0 <cg34234@gmail.com>
2025-11-20 20:35:35 -0500

    se realiza ejercicio 9

---
+++ ejercicio 9/ordenAlfabetico.js
@@ -0,0 +1,6 @@
+let estudiantes = ["Zapata", "Alvarez", "Gomez", "Bernal"];
+
+// 1. Ordenar alfabéticamente
+estudiantes.sort();
+
+console.log(estudiantes);  // ["Alvarez", "Bernal", "Gomez", "Zapata"]
