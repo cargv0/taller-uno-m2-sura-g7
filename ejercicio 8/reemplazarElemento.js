05699b20913f8e5f3cd7440747af6d7ceccb3239
cargv0 <cg34234@gmail.com>
2025-11-20 20:33:11 -0500

    se realiza ejercicio 8

---
+++ ejercicio 8/reemplazarElemento.js
@@ -0,0 +1,6 @@
+let productos = ["Monitor", "Teclado", "Mouse Roto", "Webcam"];
+
+// 1. Reemplazar "Mouse Roto" (posición 2) por "Mouse Nuevo"
+productos.splice(2, 1, "Mouse Nuevo");
+
+console.log(productos);  // ["Monitor", "Teclado", "Mouse Nuevo", "Webcam"]
